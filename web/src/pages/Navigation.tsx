c41c3d2d85d6cde41603b61dc7598f645f32c41d
Frederik Hørgreen <frederik@hoergreen.dk>
2023-09-18 11:45:54 +0200

    remove dark mode tags

--- web/src/pages/Navigation.tsx
+++ web/src/pages/Navigation.tsx
@@ -26,16 +26,16 @@ export default function Navigation(props: { authed: boolean, account?: Account }
     }, [props.account]);
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0 z-10">
+        <nav className="bg-white border-gray-200 sticky top-0 z-10">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <NavLink to={props.authed ? "/" : "#"} className="flex items-center">
                     <img src={knife_fork_plate} className="h-8 mr-3" alt="Flowbite Logo"/>
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eat Score</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap">Eat Score</span>
                 </NavLink>
                 <button
                     data-collapse-toggle="navbar-default"
                     type="button"
-                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                     aria-controls="navbar-default"
                     aria-expanded="false"
                 >
@@ -45,7 +45,7 @@ export default function Navigation(props: { authed: boolean, account?: Account }
                     </svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <div className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <div className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
 
                         {props.authed && links.map((link, index) => {
                             return (
