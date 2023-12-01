1e44a12b4e9b19de1c24ed94287af6347cb2a11f
Frederik Hørgreen <frederik@hoergreen.dk>
2023-12-01 22:55:30 +0100

    we do a little charting

--- web/src/pages/ShowVenuePage.tsx
+++ web/src/pages/ShowVenuePage.tsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 import {fetchItemsByVenueId, fetchVenueById, Item, Venue} from "../request"
 import DisplayVenue from "../components/DisplayVenue"
 import {useNavigate, useParams} from "react-router-dom"
 import CreateItem from "../components/CreateItem"
 import ListItems from "../components/ListItems"
+import moment from "moment";
+import {Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 
 export default function ShowVenuePage() {
     let {venue_id} = useParams()
@@ -27,7 +29,6 @@ export default function ShowVenuePage() {
         })
     }, [venue_id])
 
-
     const createItem = (item_id: number) => {
         if (venue === null) {
             return
@@ -35,6 +36,24 @@ export default function ShowVenuePage() {
 
         navigate(`/venues/${venue.venue_id}/items/${item_id}`)
     }
+
+    const data = useMemo(() => {
+        if (items === null) {
+            return null
+        }
+
+        if (venue_id as unknown as number != 7) {
+            return null
+        }
+
+        return items.reverse().map((item) => {
+            return {
+                created_at: moment(item.item_created_at).format("YYYY-MM-DD"),
+                name: item.item_name,
+                value: item.avg_item_rating_value,
+            }
+        })
+    }, [items])
 
     if (venue === null) {
         return (
@@ -45,6 +64,26 @@ export default function ShowVenuePage() {
     return (
         <div className={"space-y-4"}>
             <DisplayVenue venue={venue}/>
+
+            {data !== null &&
+                <div className={"box"} style={{height: "300px", width: "100%"}}>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <LineChart
+                            data={data}
+                            margin={{
+                                right: 20,
+                                left: 20,
+                            }}
+                        >
+                            <XAxis dataKey="name" hide={true}/>
+                            <YAxis type="number" domain={[0, 10]} hide={true}/>
+                            <Tooltip/>
+                            {/*<Legend />*/}
+                            <Line type="monotone" dataKey="value" stroke="#82ca9d"/>
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            }
 
             <div className={"box"}>
                 <CreateItem venue_id={venue.venue_id} onSubmit={createItem}/>
